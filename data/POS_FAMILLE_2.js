4fed10f383755b75ed6c5ac0fadeaf096eab277a
CARTOGIS33 <61816683+CARTOGIS33@users.noreply.github.com>
2021-04-07 13:45:00 +0200

    Add files via upload

---
+++ data/POS_FAMILLE_2.js
@@ -0,0 +1,11 @@
+var json_POS_FAMILLE_2 = {
+"type": "FeatureCollection",
+"name": "POS_FAMILLE_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "points": "Papy_J-J", "photo": "<img src = \"https:\/\/wtf.roflcopter.fr\/pics\/R4zO1Qrw\/B95uFFTO.JPG\"  width=\"300\" height=\"200\" alt=\"Alias Name\"\/>" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 6.234409578774971, 43.142613294608665 ] ] } },
+{ "type": "Feature", "properties": { "points": "Laurence", "photo": "<img src = \"https:\/\/wtf.roflcopter.fr\/pics\/P0RUzijM\/bFuHDE7S.JPG\"  width=\"300\" height=\"200\" alt=\"Alias Name\"\/> <img src = \"https:\/\/wtf.roflcopter.fr\/pics\/pQApFbjG\/jwhgkc0l.JPG\"  width=\"300\" height=\"200\" alt=\"Alias Name\"\/>" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -0.700608054787396, 44.791101522937979 ] ] } },
+{ "type": "Feature", "properties": { "points": "Emeline", "photo": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.998311221532978, 43.713621642597069 ] ] } },
+{ "type": "Feature", "properties": { "points": "Manon", "photo": "<img src = \"https:\/\/wtf.roflcopter.fr\/pics\/random?i=NFLtRcD8\/14sW2DW6\"  width=\"300\" height=\"200\" alt=\"Alias Name\"\/>" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 1.448218738793646, 43.647597227515163 ] ] } }
+]
+}
